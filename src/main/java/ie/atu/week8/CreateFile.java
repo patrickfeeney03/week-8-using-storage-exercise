d31eb527bb1315e9ae1fe18e173888f369ea9b03
g00409592 <g00409592@atu.ie>
2022-11-15 15:01:47 +0000

    Prompt user for path to file and print the contents of the file.

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -4,20 +4,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) {
 
         {
-            File myFile = new File("MyFile.txt");
-            System.out.println("My file is located at " + myFile.getAbsolutePath());
-
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter the name of the file that you want to print: ");
+            String filename = keyboard.nextLine();
             try{
-                PrintWriter myFilePrinter = new PrintWriter(
-                        new FileWriter(myFile, true));
-
-                myFilePrinter.println("My teacher is Paul Lenon.");
-                myFilePrinter.close();
+                Scanner inputFile = new Scanner(new File(filename));
+                while (inputFile.hasNextLine()) {
+                    System.out.println(inputFile.nextLine());
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
